8d205f5cdaa090a68b866572a7294bb7fca19e40
Aslan Lee <aslanlee@aslans-macbook-pro.local>
2018-07-12 10:07:52 -0500

    Created lose zone

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -13,12 +13,14 @@ class GameScene: SKScene {
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
     var brick = SKSpriteNode()
+    var loseZone = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         createBackground()
         makeBall()
         makePaddle()
         makeBrick()
+        makeLoseZone()
     }
     func createBackground() {
         let stars = SKTexture(imageNamed: "stars")
@@ -74,5 +76,13 @@ class GameScene: SKScene {
         brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
         brick.physicsBody?.isDynamic = false
         addChild(brick)
+    }
+    func makeLoseZone() {
+        loseZone = SKSpriteNode(color: UIColor.red, size: CGSize(width: frame.width, height: 50))
+        loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
+        loseZone.name = "loseZone"
+        loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
+        loseZone.physicsBody?.isDynamic = false
+        addChild(loseZone)
     }
 }
